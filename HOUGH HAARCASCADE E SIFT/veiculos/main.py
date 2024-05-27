5bf7a2fbc1c82378991c422fc0eac6e79e657cde
Hazielnetto <hazielnetto@gmail.com>
2024-05-27 17:26:51 -0300

    aumentar janela

--- HOUGH HAARCASCADE E SIFT/veiculos/main.py
+++ HOUGH HAARCASCADE E SIFT/veiculos/main.py
@@ -1,46 +1,26 @@
 # Autores: Haziel Netto e Vinicius Zoz
 
 import cv2
-import numpy as np
+import numpy as np 
 
-# Carrega o classificador Haar Cascade para detecção de veículos
-classifier = cv2.CascadeClassifier('HOUGH HAARCASCADE E SIFT/veiculos/cars.xml')
-historicoDetect = []
-frameCount = 0 
-
-def destacaCarros(frame, windowSize=2, drawInterval=2):
+def destacaCarros(frame):
     """
     Realça os veículos detectados no frame com retângulos.
     
     Argumentos:
     frame (array): O frame da imagem a ser processado.
-    windowSize (int): O número de frames anteriores a considerar para suavização.
-    drawInterval (int): O intervalo de frames para desenhar os retângulos.
     
     Returna:
     frame (array): O frame com os veículos detectados destacados.
     """
-    global historicoDetect    
-    global frameCount 
-
-    imgGray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-
-    if frameCount % drawInterval == 0:
-        carros = classifier.detectMultiScale(imgGray, scaleFactor=1.1, 
-                                                        minNeighbors=3, 
-                                                        minSize=(20, 20))        
-        historicoDetect.append(carros)
     
-    if len(historicoDetect) > windowSize:
-        historicoDetect = historicoDetect[-windowSize:]
-    
-    carrosSuavizados = [tuple(map(int, p)) for historico in historicoDetect 
-                                            for p in historico]
-    
-    for (x, y, w, h) in carrosSuavizados:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), color=(0, 255, 0), thickness=1)
+    carros = classifier.detectMultiScale(frame, scaleFactor=1.1,                                                        
+                                                minNeighbors=2, 
+                                                minSize=(40, 40))  
+    for (x, y, w, h) in carros:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), color=(0, 255, 0), thickness=2)
 
-    frameCount += 1
+
     return frame
 
 def main(caminhoVideo):
@@ -50,22 +30,31 @@ def main(caminhoVideo):
     Argumentos:
     caminhoVideo (str): O caminho do arquivo de vídeo a ser processado.
     """
-    video = cv2.VideoCapture(caminhoVideo)
+    video = cv2.VideoCapture(caminhoVideo)  
+    w, h, fps = (int(video.get(cv2.CAP_PROP_FRAME_WIDTH) * 2),
+            int(video.get(cv2.CAP_PROP_FRAME_HEIGHT) * 2),
+            int(video.get(cv2.CAP_PROP_FPS)))
+    
+    fps = int(1000 / fps)     
 
-    while video.isOpened():        
-        ret, frame = video.read()
-        if not ret:
-            break
-
-        frame = destacaCarros(frame)
-        cv2.imshow('Video', frame)
-        
-        if cv2.waitKey(30) == ord('q'): # Tecla para interromper a execução
+    while video.isOpened():
+        if not cv2.waitKey(fps) & 0xFF == ord('q'):      
+            ret, frame = video.read()
+            if not ret:
+                break
+            frame = destacaCarros(frame)            
+            cv2.imshow('Video', cv2.resize(frame, (w, h)))
+        else:
             break
 
     video.release()
     cv2.destroyAllWindows()
 
 if __name__ == '__main__':
-    caminhoVideo = 'HOUGH HAARCASCADE E SIFT/veiculos/cars.avi'
+    classifier = cv2.CascadeClassifier('HOUGH HAARCASCADE E SIFT/veiculos/cars.xml')
+    frameC = 0
+    kernelMorph = cv2.getStructuringElement(cv2.MORPH_RECT, (7, 7))
+    kernel = (12,12)
+    caminhoVideo = 'HOUGH HAARCASCADE E SIFT/veiculos/cars.mp4'
+    
     main(caminhoVideo)
